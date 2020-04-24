17821cc391335e94542ca78410562bfaf428076d
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-24 00:02:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,15 +5,16 @@ class Dog
     @name = name
     @@all << self
   end
-  def save
+  
   def self.all
     @@all
   end
+  
   def self.clear_all
     @@all.clear
   end
 
-  def self.print_all
+  def print_all
     @@all.each do |name|
       puts "#{name}"
     end
